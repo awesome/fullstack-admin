b658d64a903abe1059c46e6e78fae2e9bac5c8c6
mcasimir <maurizio.cas@gmail.com>
2012-08-13 17:43:34 +0200

    commit-1344872614

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -2,4 +2,8 @@ if Object.const_defined?("Ckeditor")
   Ckeditor.setup do |config|
     require "ckeditor/orm/active_record"
   end
+
+  Ckeditor::ApplicationController.class_eval do
+    authorize(:scope => :admin)
+  end
 end
